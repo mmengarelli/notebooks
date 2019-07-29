166a916e4fcd6649303b9787738cbeabe321d255
Michael Mengarelli <mengam01@gmail.com>
2019-07-29 17:06:11 -0500

    Rev 1 intial release

---
+++ amplitude/CI_CD_Enablement.py
@@ -0,0 +1,25 @@
+# Databricks notebook source
+# MAGIC %md #### Browse mock **Amplitude** data
+
+# COMMAND ----------
+
+# DBTITLE 1,DBFS Mounts on S3
+# MAGIC %fs mounts
+
+# COMMAND ----------
+
+# MAGIC %fs ls /mnt/mikem/amplitude
+
+# COMMAND ----------
+
+df = spark.read.option("multiline", "true").json("/mnt/mikem/amplitude/")
+display(df)
+
+# COMMAND ----------
+
+newDF = df.withColumnRenamed("$insert_id", "id").drop("$schema").drop("event_properties")
+newDF.write.format("delta").mode("overwrite").saveAsTable("mikem.amplitude")
+
+# COMMAND ----------
+
+# MAGIC %sql select * from mikem.amplitude
