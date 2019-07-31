0a700e8eb43a8f004a2b6f77ac5c21a70c23000b
mmengarelli <michael.mengarelli@databricks.com>
2019-07-31 13:26:34 -0700

    i am done

--- amplitude/CI_CD_Enablement.py
+++ amplitude/CI_CD_Enablement.py
@@ -12,16 +12,23 @@
 
 # COMMAND ----------
 
+# MAGIC %fs head /mnt/mikem/amplitude/amplitude1.json
+
+# COMMAND ----------
+
 df = spark.read.option("multiline", "true").json("/mnt/mikem/amplitude/")
-#display(df)
+display(df)
 
 # COMMAND ----------
 
 newDF = df.withColumnRenamed("$insert_id", "id").drop("$schema").drop("event_properties")
 newDF.write.format("delta").mode("overwrite").saveAsTable("mikem.amplitude")
 
-print("heya world")
+print("hello world")
 
 # COMMAND ----------
 
 # MAGIC %sql select * from mikem.amplitude
+
+# COMMAND ----------
+
