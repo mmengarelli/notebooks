d24b22a914ed7d2fd73c68a69daccf0fe9351a7c
Michael Mengarelli <mengam01@gmail.com>
2019-09-04 17:21:44 -0500

    Update: AnalyzingFlightDatawithDatabricksDelta.scala DeltaLakeWebinar_AnalyzingFlightData.scala Git_Test.scala MortgageDataAnalysis.scala PredictingDiabetes_KMeans.py PublicDatasets.scala PyBotoInitScript.py README.md ScooterStreamDemo.scala Scratch.scala Sommelier.py SommelierDataEngineering.py WikipediaEdits_KafkaStreamingDelta.scala WineSnob.py amplitude

---
+++ PublicDatasets.scala
@@ -0,0 +1,15 @@
+// Databricks notebook source
+// MAGIC %md
+// MAGIC * https://archive.ics.uci.edu/ml/datasets/Census+Income
+// MAGIC * https://catalog.data.gov/dataset?tags=mortgages
+// MAGIC * https://github.com/udacity/data-analyst
+// MAGIC * https://aws.amazon.com/datasets/
+// MAGIC * https://github.com/databricks/Spark-The-Definitive-Guide/tree/master/data
+// MAGIC * https://data.sfgov.org/Public-Safety/Police-Department-Incidents/tmnf-yvry
+// MAGIC * https://earthquake.usgs.gov/earthquakes/feed/v1.0/csv.php
+// MAGIC * http://archive.ics.uci.edu/ml/datasets/Wine+Quality
+// MAGIC * https://data.medicare.gov/Physician-Compare/Physician-Compare-2015-Group-Public-Reporting-Pati/2x5u-g5ws
+// MAGIC * http://www.modelingonlineauctions.com/datasets
+// MAGIC * http://www.dtic.upf.edu/~ocelma/MusicRecommendationDataset/lastfm-1K.html
+// MAGIC * https://gist.github.com/entaroadun/1653794
+// MAGIC * https://github.com/awesomedata/awesome-public-datasets
