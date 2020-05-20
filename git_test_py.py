1a9b05688e84eea7154db1f8f93088ef08f871fe
Michael Mengarelli <mengam01@gmail.com>
2020-05-20 12:10:23 -0500

    rev

--- git_test_py.py
+++ git_test_py.py
@@ -1,12 +1,2 @@
 # Databricks notebook source
-print("Hello from Databricks")
-
-# COMMAND ----------
-
-df = spark.read.option("header",True).csv("/databricks-datasets/asa/airlines/") \
-  .select("FlightNum", "Origin", "Dest", "DepTime", "ArrTime", "AirTime", "Distance") \
-  .limit(10) \
-  .show()
-
-print("adding more")
-
+println("hello world")
