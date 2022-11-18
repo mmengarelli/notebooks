b91dcf75d21255bff640d501ee9d85f6a63ae985
Michael Mengarelli <mengam01@gmail.com>
2022-11-18 11:41:23 -0500

    Update: .gitignore 02-Experiment-Tracking.py AnalyzingFlightDatawithDatabricksDelta.scala DeltaEnablement DeltaLakeWebinar_AnalyzingFlightData.scala Git_Test.scala LearningCurves.py LendingClub_DL LightPipelines.ipynb ML MortgageDataAnalysis.scala ParallelTraining3.py PredictingDiabetes_KMeans.py README.md Sommelier.py SommelierDataEngineering.py Structured Streaming (1) WikipediaEdits_KafkaStreamingDelta.scala WineSnob.py XMLParsing build.sbt git_test_py.py notebooks project src test.txt wind_turbine wind_turbine_gbt.py

---
+++ notebooks/CommonSetttings.py
@@ -0,0 +1,37 @@
+# Databricks notebook source
+pip install git+https://github.com/databricks/databricks-cli.git@tunneling-cli
+
+# COMMAND ----------
+
+spark.conf.set("spark.sql.autoBroadcastJoinThreshold", 1024*1024*1024)
+spark.conf.set("spark.sql.shuffle.partitions", sc.defaultParallelism)
+spark.conf.set( "spark.sql.shuffle.partitions", "auto" )
+
+sc.setJobDescription("JOB_DESCRIPTION") # ?? not sure if used
+spark.conf.set("spark.job.description", "MIKE_TEST_JOB")
+
+sc.setLocalProperty("callSite.short", "STAGE1")
+sc.setLocalProperty("callSite.long", "STAGE2")
+
+sc.defaultParallelism
+
+# COMMAND ----------
+
+df = spark.range(10000).toDF("id").repartition(16)
+display(df)
+
+# COMMAND ----------
+
+spark.conf.set("spark.databricks.io.cache.enabled", True)
+
+# Set the following on the clusters
+spark.databricks.io.cache.maxDiskUsage 50g
+spark.databricks.io.cache.maxMetaDataCache 1g
+spark.databricks.io.cache.compression.enabled false
+
+# COMMAND ----------
+
+spark.conf.set("mapreduce.fileoutputcommitter.marksuccessfuljobs", "false")
+spark.conf.set("parquet.enable.summary-metadata", "false")
+spark.conf.set("spark.hadoop.mapreduce.fileoutputcommitter.marksuccessfuljobs", "false")
+spark.conf.set("spark.databricks.io.directoryCommit.createSuccessFile", "false")
